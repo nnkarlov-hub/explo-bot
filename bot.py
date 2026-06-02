afdec591de1b27904d2a25702b9c30cb0397e6fb
nnkarlov-hub <nnkarlov@gmail.com>
2026-06-02 15:08:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8880597358:AAH1Q7K1I-paHqS-Iddp7A1NeBbZWsU9ap0")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def explo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     value = random.randint(0, 100)
